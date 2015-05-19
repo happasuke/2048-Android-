d16aa7ac6f8fdb8648d9cec5c4c8967944ae9ac9
happasuke <ysis0702@gmail.com>
2015-05-19 17:10:20 +0900

    sleepのエラー発生時の処理の追加

--- make2048/app/src/main/java/com/example/make2048/MainActivity.java
+++ make2048/app/src/main/java/com/example/make2048/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity implements alias{
             try{
                 Thread.sleep(sleep_time);
             }catch(Exception e){
-
+                Log.e("donatu","MainActivity Thread.sleep");
             }
         }
 
